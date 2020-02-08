20c8a0ce48aee56c0e5ba642227c518eb8c3cc0c
larikkai <kai.larikka@kolumbus.fi>
2020-02-08 18:58:43 +0200

    Attach frontend to backend

---
+++ build/precache-manifest.c85c3934c5e14e5f177908d3d4fd1d67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "071b219eac140641bd873388c14909bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc05b915ff36ab57fc4d",
+    "url": "/static/css/main.651f8f98.chunk.css"
+  },
+  {
+    "revision": "8514db5cee128984cd03",
+    "url": "/static/js/2.16d1eaee.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.16d1eaee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc05b915ff36ab57fc4d",
+    "url": "/static/js/main.30c0d956.chunk.js"
+  },
+  {
+    "revision": "cf9fd95ce589cae23005",
+    "url": "/static/js/runtime-main.2e9fb530.js"
+  }
+]);
--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :p
 
 app.use(cors())
 
+app.use(express.static('build'))
+
 let persons = [
     {
         name: "Arto Hellas",
